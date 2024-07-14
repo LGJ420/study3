60315ff147a7b051e17bcc9774720570fd45d90f
DESKTOP-B3EHO2L\xtcse <leegj420@gmail.com>
2024-07-14 22:13:28 +0900

    7.5 수정/생성 기능 추가하기

--- src/main/java/com/example/developer/controller/BlogViewController.java
+++ src/main/java/com/example/developer/controller/BlogViewController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.developer.domain.Article;
 import com.example.developer.dto.ArticleListViewResponse;
@@ -40,5 +41,20 @@ public class BlogViewController {
         model.addAttribute("article", new ArticleViewResponse(article));
 
         return "article";
+    }
+
+
+    @GetMapping("/new-article")
+    public String newArticle(@RequestParam(required = false) Long id, Model model){
+
+        if (id == null){
+            model.addAttribute("article", new ArticleViewResponse());
+        }
+        else{
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+
+        return "newArticle";
     }
 }
