5f22d6ef9cadc5989768d8c07245487e5add594e
DESKTOP-B3EHO2L\xtcse <leegj420@gmail.com>
2024-07-14 21:37:19 +0900

    7.3 블로그 글 뷰 구현하기

--- src/main/java/com/example/developer/controller/BlogViewController.java
+++ src/main/java/com/example/developer/controller/BlogViewController.java
@@ -5,8 +5,11 @@ import java.util.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.example.developer.domain.Article;
 import com.example.developer.dto.ArticleListViewResponse;
+import com.example.developer.dto.ArticleViewResponse;
 import com.example.developer.service.BlogService;
 
 import lombok.RequiredArgsConstructor;
@@ -27,5 +30,15 @@ public class BlogViewController {
         model.addAttribute("articles", articles);
 
         return "articleList";
+    }
+
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model){
+
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
     }
 }
--- src/main/java/com/example/developer/domain/Article.java
+++ src/main/java/com/example/developer/domain/Article.java
@@ -1,8 +1,15 @@
 package com.example.developer.domain;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import jakarta.persistence.*;
 import lombok.*;
 
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -18,6 +25,14 @@ public class Article {
 
     @Column(name = "content", nullable = false)
     private String content;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 
     @Builder
     public Article(String title, String content){
