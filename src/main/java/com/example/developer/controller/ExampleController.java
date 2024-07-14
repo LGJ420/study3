00146413943ae39c4a0c24b7283c89d17bc8f3e6
DESKTOP-B3EHO2L\xtcse <leegj420@gmail.com>
2024-07-14 18:39:29 +0900

    7.1 사전 지식 : 타임리프

---
+++ src/main/java/com/example/developer/controller/ExampleController.java
@@ -0,0 +1,42 @@
+package com.example.developer.controller;
+
+import java.util.*;
+import java.time.*;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Controller
+public class ExampleController {
+    
+    @GetMapping("/thymeleaf/example")
+    public String thymeleafExample(Model model){
+
+        Person examplPerson = new Person();
+        examplPerson.setId(1L);
+        examplPerson.setName("홍길동");
+        examplPerson.setAge(11);
+        examplPerson.setHobbies(List.of("운동", "독서"));
+
+        model.addAttribute("person", examplPerson);
+        model.addAttribute("today", LocalDate.now());
+
+        return "example";
+    }
+
+
+
+    @Getter
+    @Setter
+    class Person {
+
+        private Long id;
+        private String name;
+        private int age;
+        private List<String> hobbies;
+    }
+}
