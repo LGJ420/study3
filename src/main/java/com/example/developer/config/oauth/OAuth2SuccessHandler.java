a3209697dad33a98ce14ef7d84f049078522043b
DESKTOP-B3EHO2L\xtcse <leegj420@gmail.com>
2024-07-19 19:12:30 +0900

    10.3 스프링 시큐리티로 OAuth2를 구현하고 적용하기 fixed

--- src/main/java/com/example/developer/config/oauth/OAuth2SuccessHandler.java
+++ src/main/java/com/example/developer/config/oauth/OAuth2SuccessHandler.java
@@ -1,8 +1,8 @@
 package com.example.developer.config.oauth;
 
-import java.io.IOException;
-import java.time.Duration;
-
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
@@ -16,14 +16,13 @@ import com.example.developer.repository.RefreshTokenRepository;
 import com.example.developer.service.UserService;
 import com.example.developer.util.CookieUtil;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
+import java.io.IOException;
+import java.time.Duration;
 
 @RequiredArgsConstructor
 @Component
-public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler{
-    
+public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
     public static final String REFRESH_TOKEN_COOKIE_NAME = "refresh_token";
     public static final Duration REFRESH_TOKEN_DURATION = Duration.ofDays(14);
     public static final Duration ACCESS_TOKEN_DURATION = Duration.ofDays(1);
@@ -35,18 +34,14 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler{
     private final UserService userService;
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request,
-        HttpServletResponse response,
-        Authentication authentication) throws IOException {
-
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
-
-        User user = userService.findByEmail((String) oAuth2User.getAttributes().get("emial"));
+        User user = userService.findByEmail((String) oAuth2User.getAttributes().get("email"));
 
         String refreshToken = tokenProvider.generateToken(user, REFRESH_TOKEN_DURATION);
         saveRefreshToken(user.getId(), refreshToken);
         addRefreshTokenToCookie(request, response, refreshToken);
-        
+
         String accessToken = tokenProvider.generateToken(user, ACCESS_TOKEN_DURATION);
         String targetUrl = getTargetUrl(accessToken);
 
@@ -55,40 +50,30 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler{
         getRedirectStrategy().sendRedirect(request, response, targetUrl);
     }
 
-
-    private void saveRefreshToken(Long userId, String newRefreshToken){
-
+    private void saveRefreshToken(Long userId, String newRefreshToken) {
         RefreshToken refreshToken = refreshTokenRepository.findByUserId(userId)
-            .map(entity -> entity.update(newRefreshToken))
-            .orElse(new RefreshToken(userId, newRefreshToken));
+                .map(entity -> entity.update(newRefreshToken))
+                .orElse(new RefreshToken(userId, newRefreshToken));
 
         refreshTokenRepository.save(refreshToken);
     }
 
-
-    private void addRefreshTokenToCookie(HttpServletRequest request,
-        HttpServletResponse response,
-        String refreshToken){
-
+    private void addRefreshTokenToCookie(HttpServletRequest request, HttpServletResponse response, String refreshToken) {
         int cookieMaxAge = (int) REFRESH_TOKEN_DURATION.toSeconds();
-        CookieUtil.deleteCookie(request, response, REFRESH_TOKEN_COOKIE_NAME);;
+
+        CookieUtil.deleteCookie(request, response, REFRESH_TOKEN_COOKIE_NAME);
         CookieUtil.addCookie(response, REFRESH_TOKEN_COOKIE_NAME, refreshToken, cookieMaxAge);
     }
 
-
-    private void clearAuthenticationAttributes(HttpServletRequest request,
-        HttpServletResponse response){
-
+    private void clearAuthenticationAttributes(HttpServletRequest request, HttpServletResponse response) {
         super.clearAuthenticationAttributes(request);
         authorizationRequestRepository.removeAuthorizationRequestCookies(request, response);
     }
 
-
-    private String getTargetUrl(String token){
-
+    private String getTargetUrl(String token) {
         return UriComponentsBuilder.fromUriString(REDIRECT_PATH)
-            .queryParam("token", token)
-            .build()
-            .toUriString();
+                .queryParam("token", token)
+                .build()
+                .toUriString();
     }
 }
--- src/main/java/com/example/developer/config/oauth/OAuth2UserCustomService.java
+++ src/main/java/com/example/developer/config/oauth/OAuth2UserCustomService.java
@@ -1,7 +1,6 @@
 package com.example.developer.config.oauth;
 
-import java.util.Map;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -11,38 +10,35 @@ import org.springframework.stereotype.Service;
 import com.example.developer.domain.User;
 import com.example.developer.repository.UserRepository;
 
-import lombok.RequiredArgsConstructor;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Service
-public class OAuth2UserCustomService extends DefaultOAuth2UserService{
-    
+public class OAuth2UserCustomService extends DefaultOAuth2UserService {
+
     private final UserRepository userRepository;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-
         OAuth2User user = super.loadUser(userRequest);
         saveOrUpdate(user);
 
         return user;
-    } 
-
+    }
 
     private User saveOrUpdate(OAuth2User oAuth2User) {
-
         Map<String, Object> attributes = oAuth2User.getAttributes();
+
         String email = (String) attributes.get("email");
         String name = (String) attributes.get("name");
 
         User user = userRepository.findByEmail(email)
-            .map(entity -> entity.update(name))
-            .orElse(User.builder()
-                .email(email)
-                .nickname(name)
-                .build());
-        
-        return userRepository.save(user);
+                .map(entity -> entity.update(name))
+                .orElse(User.builder()
+                        .email(email)
+                        .nickname(name)
+                        .build());
 
+        return userRepository.save(user);
     }
 }
